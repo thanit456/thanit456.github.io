fe28edd3efaebf0172a820ea614f163b4ac7a51a
thanit456 <too_w_o@hotmail.com>
2020-05-19 18:11:43 +0700

    go to default

--- src/components/header.js
+++ src/components/header.js
@@ -1,64 +1,38 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import BackgroundImage from "gatsby-background-image"
-import { graphql } from "gatsby"
 
-import "../css/background-image.css"
-// import { rhythm } from "../utils/typography"
-//www.google.com/search?q=background+image+css+gatsby&oq=background+image+css+gatsby&aqs=chrome.0.0l2.19946j1j4&sourceid=chrome&ie=UTF-8#kpvalbx=_IVG8XvyfNYP49QO15a6gCA30
-
-https: const Header = ({ setTitle }) => (
+const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `blue`,
-      // backgroundImage: new Image(`../images/line_network_bg.jpg`),
+      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
   >
-    <BackgroundImage
-      className="masterthead"
-      fluid={props.data.indexImage.childImageSharp.fluid}
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
     >
-      <div className="black-overlay">
-        <div
-          className="content-box"
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
           style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `1.45rem 1.0875rem`,
+            color: `white`,
+            textDecoration: `none`,
           }}
         >
-          <h1 style={{ margin: 0 }}>
-            <Link
-              to="/"
-              style={{
-                color: `white`,
-                textDecoration: `none`,
-              }}
-            >
-              {props.siteTitle}
-            </Link>
-          </h1>
-          <p>
-            <Link
-              to={`/about/`}
-              style={{
-                float: `right`,
-              }}
-            >
-              About
-            </Link>
-          </p>
-        </div>
-      </div>
-    </BackgroundImage>
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
   </header>
 )
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-  image: PropTypes.image,
 }
 
 Header.defaultProps = {
@@ -66,15 +40,3 @@ Header.defaultProps = {
 }
 
 export default Header
-
-export const pageQuery = graphql`
-  query {
-    indexImage: file(relativePath: { eq: "line_network_bg.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 1800) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
